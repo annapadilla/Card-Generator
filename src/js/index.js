e33c4cb94998c91628c3e38c62c3d1ef07b16422
annapadilla <anna.helin@gmail.com>
2019-07-25 20:13:50 +0000

    all done

--- src/js/index.js
+++ src/js/index.js
@@ -6,6 +6,37 @@ import "../assets/img/4geeks.ico";
 //import 'breathecode-dom'; //DOM override to make JS easier to use
 import "../style/index.scss";
 
-window.onload = function() {
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  let excuse = "";
+  let arrayNumbers = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+  let arraySuite = ["\u2660", "\u2665", "\u2666", "\u2663"];
+  let randomValue1 =
+    arrayNumbers[Math.floor(Math.random() * arrayNumbers.length)];
+  let randomValue2 = arraySuite[Math.floor(Math.random() * arraySuite.length)];
+
+  if (randomValue2 === "\u2665" || randomValue2 === "\u2666") {
+    document.querySelector(".top-suit").className = "top-suit red";
+    document.querySelector(".bottom-suit").className = "bottom-suit red";
+  }
+
+  console.log(randomValue2);
+  console.log(randomValue1);
+  //excuse = randomValue1 + " " + randomValue2;
+  document.querySelector("#number").innerHTML = randomValue1;
+  document.querySelector(".top-suit").innerHTML = randomValue2;
+  document.querySelector(".bottom-suit").innerHTML = randomValue2;
 };
